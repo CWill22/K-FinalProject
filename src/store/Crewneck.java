158825af0a058b6bcd39d87ed44384085810e1d2
Alasdair Orr <aroqkv@umsystem.edu>
2024-04-11 12:36:27 -0500

    fix the product

--- src/store/Crewneck.java
+++ src/store/Crewneck.java
@@ -2,6 +2,12 @@ package store;
 
 public class Crewneck extends Product {
     public Crewneck(String name, String brand, String color, String size, String material, String gender, double price, int quantity){
-        super();
+        super(name, brand, color, size, material, gender, price, quantity);
+    }
+
+    @Override
+    public void displayDetails() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayDetails'");
     }
 }
--- src/store/Product.java
+++ src/store/Product.java
@@ -1,12 +1,11 @@
 package store;
-
 public abstract class Product {
-    private String name;
-    private String brand;
-    private double price;
-    private int quantity;
-    private String size;
-    private String color;
+    protected String name;
+    protected String brand;
+    protected double price;
+    protected int quantity;
+    protected String size;
+    protected String color;
 
     public Product(String name, String brand, double price, int quantity, String size, String color) {
         this.name = name;
@@ -17,33 +16,47 @@ public abstract class Product {
         this.color = color;
     }
 
-    public abstract void displayDetails();
+    public Product(String name2, String brand2, String color2, String size2, String material, String gender,
+            double price2, int quantity2) {
+        //TODO Auto-generated constructor stub
+    }
 
+    public abstract void displayDetails();
+    //getters and setters
     public String getName() {
         return name;
     }
-
+    public void setName(String name) {
+        this.name = name;
+    }
     public String getBrand() {
         return brand;
     }
-
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
     public double getPrice() {
         return price;
     }
-
+    public void setPrice(double price) {
+        this.price = price;
+    }
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
     public String getSize() {
         return size;
     }
-
+    public void setSize(String size) {
+        this.size = size;
+    }
     public String getColor() {
         return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
     }
 }
