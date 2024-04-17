18692f4d5f7f305c850fc45e8dc93043ada7bb54
Alasdair Orr <aroqkv@umsystem.edu>
2024-04-16 20:00:03 -0500

    fix the subclass

--- src/store/Crewneck.java
+++ src/store/Crewneck.java
@@ -1,9 +1,21 @@
 package store;
 
 public class Crewneck extends Product {
-    public Crewneck(String name, String brand, String color, String size, String material, String gender, double price, int quantity){
-        super(name, brand, color, size, material, gender, price, quantity);
+
+    public Crewneck(String name, Brands brand, double price, int quantity, Size size, Color color, Material material, Gender gender) {
+        super(name, brand, price, quantity, size, color, material, gender);
     }
 
-   
+    @Override
+    public void displayDetails() {
+        System.out.println("Name: " + getName());
+        System.out.println("Brand: " + getBrand());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Quantity: " + getQuantity());
+        System.out.println("Size: " + getSize());
+        System.out.println("Color: " + getColor());
+        System.out.println("Material: " + getMaterial());
+        System.out.println("Gender: " + getGender());
+    }
 }
+
