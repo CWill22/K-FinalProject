cebbc09a53712d8cfd9793a5743afca99403f0bc
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-04-16 20:19:36 -0500

    add main, and update Customer

--- src/store/Customer.java
+++ src/store/Customer.java
@@ -1,5 +1,40 @@
 package store;
 
 public class Customer {
+	public String username;
+	private String password;
+	
+	//constructor for Customer (createAccount)
+	public Customer(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	///createAccount method
+	public Customer createAccount(String username, String password) {
+        Customer customer = new Customer(username, password);
+        return customer;
+    }
+	
+	
+	
+	//getter and setter methods
+	/*
+	protected String getPassword() {
+		return password;
+	}
+	protected void setPassword(String password) {
+		this.password = password;
+	}
+	*/
+	
+	//validate password
+	public boolean validateLoginCredentials(String password) {
+		return this.password.equals(password);
+	}
 
+	//deleteAccount method
+	public void deleteAccount() {
+		this.username = null;
+		this.password = null;
+	}
 }
---
+++ src/store/Main.java
@@ -0,0 +1,14 @@
+package store;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Initialize the database
+		Database db = new Database();
+		//Initialize the UserInterface
+		UserInterface ui = new UserInterface();
+		
+	}
+
+}
