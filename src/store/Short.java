93574443a208aed51c0e2c3b8355b1a0ae38ecb4
Alasdair Orr <aroqkv@umsystem.edu>
2024-04-16 19:58:56 -0500

    fix the subclasses

--- src/store/Short.java
+++ src/store/Short.java
@@ -1,14 +1,21 @@
 package store;
 
 public class Short extends Product {
-    public Short(String name2, String brand2, String color2, String size2, String material, String gender,
-            double price2, int quantity2) {
-        super(name2, brand2, color2, size2, material, gender, price2, quantity2);
-        //TODO Auto-generated constructor stub
+
+    public Short(String name, Brands brand, double price, int quantity, Size size, Color color, Material material, Gender gender) {
+        super(name, brand, price, quantity, size, color, material, gender);
     }
 
     @Override
     public void displayDetails() {
-        // Add implementation here
+        System.out.println("Name: " + getName());
+        System.out.println("Brand: " + getBrand());
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Quantity: " + getQuantity());
+        System.out.println("Size: " + getSize());
+        System.out.println("Color: " + getColor());
+        System.out.println("Material: " + getMaterial());
+        System.out.println("Gender: " + getGender());
     }
 }
+
