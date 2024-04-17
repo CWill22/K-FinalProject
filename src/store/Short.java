69afe77608f374ac6f43d516b0338d9009250d87
Alasdair Orr <aroqkv@umsystem.edu>
2024-04-16 19:35:46 -0500

    commiting

--- src/store/Short.java
+++ src/store/Short.java
@@ -1,5 +1,14 @@
 package store;
 
 public class Short extends Product {
+    public Short(String name2, String brand2, String color2, String size2, String material, String gender,
+            double price2, int quantity2) {
+        super(name2, brand2, color2, size2, material, gender, price2, quantity2);
+        //TODO Auto-generated constructor stub
+    }
 
+    @Override
+    public void displayDetails() {
+        // Add implementation here
+    }
 }
