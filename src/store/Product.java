24aa402bda801ab7185d03430a3b9a7dd6a3aa76
Alasdair Orr <aroqkv@umsystem.edu>
2024-04-16 19:54:41 -0500

    fix the naming of enum

--- src/store/Product.java
+++ src/store/Product.java
@@ -2,15 +2,15 @@ package store;
 
 public abstract class Product {
     protected String name;
-    protected String brand;
+    protected Brands brand;
     protected double price;
     protected int quantity;
-    protected String size;
-    protected String color;
+    protected Size size;
+    protected Color color;
     protected Gender gender;
     protected Material material;
 
-    public Product(String name, String brand, double price, int quantity, String size, String color, Material material, Gender gender) {
+    public Product(String name, Brands brand, double price, int quantity, Size size, Color color, Material material, Gender gender) {
         this.name = name;
         this.brand = brand;
         this.price = price;
@@ -22,52 +22,68 @@ public abstract class Product {
     }
 
     public abstract void displayDetails();
-    //getters and setters
+
+    // Getters and setters for the attributes
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public String getBrand() {
+
+    public Brands getBrand() {
         return brand;
     }
-    public void setBrand(String brand) {
+
+    public void setBrand(Brands brand) {
         this.brand = brand;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public int getQuantity() {
         return quantity;
     }
+
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-    public String getSize() {
+
+    public Size getSize() {
         return size;
     }
-    public void setSize(String size) {
+
+    public void setSize(Size size) {
         this.size = size;
     }
-    public String getColor() {
+
+    public Color getColor() {
         return color;
     }
-    public void setColor(String color) {
+
+    public void setColor(Color color) {
         this.color = color;
     }
+
     public Gender getGender() {
         return gender;
     }
+
     public void setGender(Gender gender) {
         this.gender = gender;
     }
+
     public Material getMaterial() {
         return material;
     }
+
     public void setMaterial(Material material) {
         this.material = material;
     }
