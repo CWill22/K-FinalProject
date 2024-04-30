1a82d19adb5acb3a29db6cd12ad2932ca9917641
bpenr <bpenr@BraedensPC>
2024-04-30 18:59:00 -0500

    Fixed main.

--- src/store/Main.java
+++ src/store/Main.java
@@ -5,26 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Initialize the database
-<<<<<<< HEAD
+
 		Database db = new Database();
 		//Initialize the UIManager
 		UIManager uiManager = new UIManager();
-=======
-		//Database db = new Database();
-		//Initialize the UserInterface
-		//UserInterface ui = new UserInterface();
-		
-		// Create a new LoginPage2 instance
-        LoginPage2 loginPage = new LoginPage2();
-        // Make the login page visible
-        loginPage.frame.setVisible(true);
-
->>>>>>> branch 'main' of https://github.com/CWill22/K-FinalProject
-		
 	}
-	
-	//test commit
-	//wills 2nd test commit
-	
-	//LoginPage2 lp = new LoginPage2();
 }
